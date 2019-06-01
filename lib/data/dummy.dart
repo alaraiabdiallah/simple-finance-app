c0986af555de07c411ff163e465ee980abdd44fb
Ala Rai <alaraiabdiallah@gmail.com>
2019-06-02 04:42:48 +0700

    First Init

---
+++ lib/data/dummy.dart
@@ -0,0 +1,6 @@
+import "package:finance/models/account.dart";
+
+List<Account> accounts = [
+  Account(id:1,name:"Wallet",amount: 110000),
+  Account(id:2,name:"BNI",amount: 120000),
+];
---
+++ lib/models/account.dart
@@ -0,0 +1,7 @@
+class Account {
+    Account({this.id, this.name, this.amount});
+
+    int id;
+    String name;
+    int amount;
+}
